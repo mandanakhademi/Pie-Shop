92b61eeae086758f1ea596c115f27d2733a0c972
Mandana <mandanakhademi88@gmail.com>
2019-07-12 14:15:23 +0100

    add home view model and refactor index action in home controller

--- PieShop/Controllers/HomeController.cs
+++ PieShop/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PieShop.Models;
+using PieShop.ViewModels;
 
 namespace PieShop.Controllers
 {
@@ -16,10 +17,16 @@ namespace PieShop.Controllers
         }
         public IActionResult Index()
         {
-            ViewBag.Title = "Pie overview";
 
             var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
-            return View(pies);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Wellcome to our Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
         }
     }
 }
